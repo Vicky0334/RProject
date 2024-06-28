19b4a9b82c616e7ead56d8bb6e37af4caf35bb9c
Vicky Dhote <dhoteji034@gmail.com>
2024-06-28 18:21:16 +0530

    pot

---
+++ src/components/homepage/navbar/NavLeft.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavLeft = () => {
+  return (
+    <div>
+      <h2>@Code</h2>
+    </div>
+  )
+}
+
+export default NavLeft
---
+++ src/components/homepage/navbar/NavRight.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavRight = () => {
+  return (
+    <div>
+<a href="#">Work</a>
+<a href="#">About</a>
+<a href="#" className=''>Contact</a>
+    </div>
+  )
+}
+
+export default NavRight
