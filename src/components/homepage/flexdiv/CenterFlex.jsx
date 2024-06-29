395e59bd2bdc83b8758925483a8727fa632c0920
Vicky Dhote <dhoteji034@gmail.com>
2024-06-29 21:31:49 +0530

    m

--- src/components/homepage/flexdiv/CenterFlex.jsx
+++ src/components/homepage/flexdiv/CenterFlex.jsx
@@ -5,15 +5,15 @@ const CenterFlex = () => {
   return (
     <div className='mt-78 bg-red-100 text-white relative z-10 flex items-end justify-between w-100'>
       <div className='flex bg-black items-center gap-6 px-2 py-2 pl-10 rounded-e-full'>
-        <h2 className='text-20 leading-none'>Located <br />in the <br />Netherlands</h2>
-        <div className='bg-gray-500 flex items-center justify-center h-20 w-20 rounded-full'>
-          <i className="ri-global-line text-4xl rotate-12 font-thin"></i>
+        <h2 className=' leading-none'>Located <br />in the <br />Netherlands</h2>
+        <div className='bg-gray-500 flex items-center justify-center h-19 w-19 rounded-full'>
+          <i className="ri-global-line text-5xl rotate-12 font-thin"></i>
         </div>
       </div>
-      <div className='flex flex-col gap-15 '>
-        <i class="ri-arrow-right-down-line text-5xl"></i>
-        <h2 className='text-3xl  font-normal'>FreeLance <br />Designer and Developer</h2>
-      </div>
+      <div className='flex flex-col'>
+        <i class="ri-arrow-right-down-line text-4xl"></i>
+        <h2 className='text-2xl font-normal leading-none '>FreeLance <br />Designer and Developer</h2>
+        </div>
     </div>
   )
 }
